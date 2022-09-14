17da2e85d4b84424c024f5e47181b20e165f6bdf
Himanshu <himanshu.khetwal789@gmail.com>
2022-09-14 18:43:02 +0530

    initial commit

---
+++ src/main/java/com/Bakery/major/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.Bakery.major.repository;
+
+import com.Bakery.major.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+}
